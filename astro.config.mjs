b23c7518ae34bc411b74bd996019b413326aa6d5
XiongZixiang <xzx20030915@gmail.com>
2026-09-02 16:56:29 +0800

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   // Set this to your production URL (no trailing slash)
-  site: 'https://zixiangcoder.github.io/',
+  site: 'https://zixiangcoder.github.io',
   // Set this to your site's subpath if it is NOT hosted at the domain root
   // (e.g. GitHub Pages project sites). Delete this line if your site lives at "/".
   base: '/astro-wanderer',
